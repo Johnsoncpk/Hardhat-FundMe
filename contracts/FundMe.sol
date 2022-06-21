45c69dd9dc2b9ddb70fcb596c865a297e52c14b2
Johnsonc <chong12319@gmail.com>
2022-06-21 16:37:37 +0800

    fundme

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,85 +1,67 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// Get funds from users
+// Withdraw Funds
+// Set a minium funding value in USD
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
 import "./PriceConverter.sol";
 
 error NotOwner();
 
-contract FundMe {
+contract FundMe{
+
     using PriceConverter for uint256;
 
-    mapping(address => uint256) public addressToAmountFunded;
+    // current gas 885,394
+    // constant/immutable to reduce gas
+    // 885129 -> 859146
+    // immutable 859146 -> 832016
+    uint256 public constant MINIMUM_USD = 2;
     address[] public funders;
+    address public immutable owner;
+    mapping(address => uint256) public addressToAmountFunder;
+    AggregatorV3Interface public priceFeed;
 
-    // Could we make this constant?  /* hint: no! We should make it immutable! */
-    address public /* immutable */ i_owner;
-    uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
-    
-    constructor() {
-        i_owner = msg.sender;
+    constructor(address priceFeedAddress){
+        owner = msg.sender;
+        priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
-    function fund() public payable {
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
-        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
-        addressToAmountFunded[msg.sender] += msg.value;
+    function fund() public payable{
+        //set a minimum fund
+        require(msg.value.getConversionRate(priceFeed) >= MINIMUM_USD, "Not enough value."); 
         funders.push(msg.sender);
+        addressToAmountFunder[msg.sender] = msg.value;
     }
-    
-    function getVersion() public view returns (uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-        return priceFeed.version();
+
+    receive() external payable{
+        fund();
     }
-    
-    modifier onlyOwner {
-        // require(msg.sender == owner);
-        if (msg.sender != i_owner) revert NotOwner();
-        _;
+
+    fallback() external payable{
+        fund();
     }
-    
-    function withdraw() payable onlyOwner public {
-        for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
-            address funder = funders[funderIndex];
-            addressToAmountFunded[funder] = 0;
-        }
+
+    function withdraw() public onlyOwner{
         funders = new address[](0);
-        // // transfer
+        // reset array
+        // transfer, send, call
+        // payable(msg.sender) = payable address
         // payable(msg.sender).transfer(address(this).balance);
-        // // send
+
         // bool sendSuccess = payable(msg.sender).send(address(this).balance);
         // require(sendSuccess, "Send failed");
-        // call
+        //call
         (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call failed");
     }
-    // Explainer from: https://solidity-by-example.org/fallback/
-    // Ether is sent to contract
-    //      is msg.data empty?
-    //          /   \ 
-    //         yes  no
-    //         /     \
-    //    receive()?  fallback() 
-    //     /   \ 
-    //   yes   no
-    //  /        \
-    //receive()  fallback()
 
-    fallback() external payable {
-        fund();
+    modifier onlyOwner{
+        //using customized error can reduce gas 
+        if(msg.sender != owner){
+            revert NotOwner();
+        }
+        _;
     }
-
-    receive() external payable {
-        fund();
-    }
-
 }
-
-// Concepts we didn't cover yet (will cover in later sections)
-// 1. Enum
-// 2. Events
-// 3. Try / Catch
-// 4. Function Selector
-// 5. abi.encode / decode
-// 6. Hash with keccak256
-// 7. Yul / Assembly
--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -2,16 +2,41 @@
 // main function
 // calling of main function
 
-const { networks } = require("../hardhat.config");
+const { network } = require("hardhat");
 
-// function deployFunction(hre) {
-//     console.log("hi");
-// };
-
-// module.exports.default = deployFunction;
+const { verify } = require("../utils/verify");
+const { networkConfig, developmentChains } = require("../helper-hardhat-config");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
-    const chainId = networks.config.chainId;
+    const chainId = network.config.chainId;
+
+    let ethUsdPirceFeedAddress;
+
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator");
+        ethUsdPirceFeedAddress = ethUsdAggregator.address;
+    } else {
+        ethUsdPirceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+    }
+
+    const args = [
+        ethUsdPirceFeedAddress
+    ];
+
+    //use mock
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+    if (!developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY) {
+        await verify(fundMe.address, args);
+    }
+    log("----------------");
 }
+
+module.exports.tags = ["all", "fundme"];
